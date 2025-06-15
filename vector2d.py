43033106a338245eff008ad733e42e493f10b922
Kamski <sashchuk132@gmail.com>
2025-06-15 19:09:42 +0300

    Added Vector2D class and attributes,methods

---
+++ vector2d.py
@@ -0,0 +1,26 @@
+class Vector2D:
+    def __init__(self, x, y):
+        if not isinstance(x, (int, float)):
+            raise TypeError("Координата X повинна бути числом (ціле абож дробове).")
+
+        if not isinstance(y,(int, float)):
+            raise TypeError("Координата Y повинна бути числом (ціле або дробове).")
+
+        self._x = float(x)
+        self._y = float(y)
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    def __str__(self):
+        return f"Вектор2Д: ({self._x}, {self._y})"
+
+
+
+
+
